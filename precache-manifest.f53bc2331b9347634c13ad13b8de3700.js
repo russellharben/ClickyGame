b331a63771f797cd1cd1734043f8af379f2bb0e3
Russell Harben <russellharben@gmail.com>
2019-04-15 10:17:16 -0400

    Updates

---
+++ precache-manifest.f53bc2331b9347634c13ad13b8de3700.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57c3f4550ece13071da4",
+    "url": "/ClickyGame/static/css/main.17b21160.chunk.css"
+  },
+  {
+    "revision": "57c3f4550ece13071da4",
+    "url": "/ClickyGame/static/js/main.da8fb24d.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "83c70a1c6c34325387ce",
+    "url": "/ClickyGame/static/js/2.5474d1c6.chunk.js"
+  },
+  {
+    "revision": "8377a7a8304597fb067b575133fc347c",
+    "url": "/ClickyGame/index.html"
+  }
+];
